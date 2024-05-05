57bdcfc876066306f063cfd4a5f595a1fa1cf3cd
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-05-05 23:35:34 +0530

    TMDB API is down in india and if we want to uae the app in india we have to use VPN to fetch the data

--- src/customHooks/useNowPlayingMovies.js
+++ src/customHooks/useNowPlayingMovies.js
@@ -9,7 +9,7 @@ const useNowPlayingMovies = ()=>{
     const getNowPlayingMovies = async() =>{
       const data = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=aae31bc3cfc5ef6bab2b64cc33c22174');
       const json = await data.json();
-      dispatch(addNowPlayingMovies(json.results))
+      dispatch(addNowPlayingMovies(json))
     }
   
     useEffect(()=>{
@@ -17,21 +17,21 @@ const useNowPlayingMovies = ()=>{
     },[])
 
     const getNowPlayingMovies1 = async() =>{
-      const url = 'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming?limit=20';
+      const url = 'https://imdb188.p.rapidapi.com/api/v1/getWhatsStreaming?country=US';
       const options = {
 	      method: 'GET',
 	      headers: {
-		    'X-RapidAPI-Key': '7e321a2e3dmshf92e1f9626cf012p1bbd6ejsn442803ed61e7',
-		    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		      'X-RapidAPI-Key': '7e321a2e3dmshf92e1f9626cf012p1bbd6ejsn442803ed61e7',
+		      'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
 	      }
       };
 
       try {
 	      const response = await fetch(url, options);
-	      const result = await response.text();
-	      console.log(result);
-        const json = result.json();
-        dispatch(addNowPlayingMovies(json.results));
+	      const result = await response.json();
+        dispatch(addNowPlayingMovies(result.data))
+	      // console.log(result);
+
       } catch (error) {
 	      console.error(error);
       }
