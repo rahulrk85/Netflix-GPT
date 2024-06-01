2b96e3e3fb93165ca14b3ea0747887cf5e218a39
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-06-01 17:14:59 +0530

    TMDB is BACK!

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ import { MOVIE_PATH } from '../utilities/constants'
 const MovieCard = ({poster}) => {
   if(!poster) return null;
   return (
-    <div className='w-36 md:w-48'>
+    <div className='w-36 md:w-48 cursor-pointer transition-transform duration-[0.9s] mx-auto my-0 hover:scale-100 hover:px-5 hover:w-[220px] hover:mt-3 hover:opacity-65'>
         <img className='px-2' src={poster} alt='img'/>
     </div>
   )
--- src/customHooks/usePopularMovies.js
+++ src/customHooks/usePopularMovies.js
@@ -8,15 +8,31 @@ const usePopularMovies = ()=>{
     const popularMovies = useSelector((store)=>store.movies.popularMovies);
 
     const getNowPlayingMovies = async() =>{
-      const data = await fetch('https://sidecar.tvtime.com/sidecar/tvtcached?o=https://discover.tvtime.com/v1/discover/cgw/trending');
+      const data = await fetch('https://tubitv.com/oz/videos/100020038/related?limit_resolutions=h264_1080p&limit_resolutions=h265_1080p&video_resources=hlsv6_widevine_nonclearlead&video_resources=hlsv6_playready_psshv0&video_resources=hlsv6_fairplay&video_resources=hlsv6&limit=20');
       const json = await data.json();
-      dispatch(addPopularMovies(json.data));
+      console.log(json);
+      // dispatch(addPopularMovies(json.data));
     }
+    const getNowPlayingMovies1 = async() =>{
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYWUzMWJjM2NmYzVlZjZiYWIyYjY0Y2MzM2MyMjE3NCIsInN1YiI6IjY1YzdiM2UzOThmMWYxMDE4M2Q3N2VhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hFdbESYvxa2puwiDbu-vLLfkGdhqvj9LLdCqZzQY7t4'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
+
   
     // the tmdb website is not working and the list of movies like nowplaying movies and others are not able to fetch from this website so either find another website with same api structure or build your own api i think we should use the vpn to fetch the movies of the tmdb website try once doing this and let me know if that works .dispatch(addPopularMovies(json.results));
   
     useEffect(()=>{
-     !popularMovies && getNowPlayingMovies();
+     !popularMovies && getNowPlayingMovies1();
     },[])
 };
 export default usePopularMovies;
