ec8b95e99d2b1dded392a863468145b6ea50661e
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-06-08 03:10:10 +0530

    hovering property on cards

--- src/components/SimilarMovies.js
+++ src/components/SimilarMovies.js
@@ -5,8 +5,12 @@ const SimilarMovies = ({ movieSimilar }) => {
   console.log(movieSimilar);
   const { poster_path } = movieSimilar;
   return (
-    <div className="w-[200px] p-2">
-      <img src={POSTER_PATH + poster_path} alt="image_poster" />
+    <div className="w-[200px] p-2w-36 md:w-48 cursor-pointer transition-transform duration-[0.9s] mx-auto my-0 hover:scale-100 hover:px-5 hover:w-[220px] hover:mt-3 hover:opacity-65">
+      <img
+        className="px-2"
+        src={POSTER_PATH + poster_path}
+        alt="image_poster"
+      />
     </div>
   );
 };
