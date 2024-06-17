2a37e79239f03743c442136792044abb125b897d
Rahul Kulkarni <rahulrkulkarni20@gmail.com>
2024-06-17 21:18:13 +0530

    rerendering the background video part to show the trailer video

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -5,51 +5,36 @@ import ShimmerUi from "./ShimmerUi";
 import DupliHeader from "./DupliHeader";
 import useSimilarmovies from "../customHooks/useSimilarMovies";
 import SimilarMovies from "./SimilarMovies";
-import { useDispatch, useSelector } from "react-redux";
-import BackgroungVideo from "./BackgroungVideo";
-import { addmoviePageTrailer } from "../utilities/moviesSlice";
-import { useState } from "react";
 
 const MovieDetail = () => {
-  const Trailer = useSelector((store) => store.movies.moviePageTrailer);
-  const [TrailerVideo, setTrailerVideo] = useState(false);
-  const dispatch = useDispatch();
   const { id } = useParams();
   const data = useMoviePage(id);
   const movieSimilar = useSimilarmovies(id);
   if (movieSimilar === null) return;
-  // console.log(movieSimilar);
   if (data === null) return <ShimmerUi />;
   const { backdrop_path, title, overview, vote_average } = data;
-  const TimeOut = setTimeout(() => setTrailerVideo(true), 7000);
 
   return (
     <div className="">
       <DupliHeader />
-      {TrailerVideo ? (
-        <BackgroungVideo id={id} />
-      ) : (
-        <>
-          <div className="text-white text-3xl w-full h-[830px] absolute  bg-gradient-to-t from-black">
-            <div className="mt-[250px] ml-24">
-              <h1 className="font-mono ">HD 2024</h1>
-              <h1 className="font-serif text-6xl py-6">{title}</h1>
-              <h1 className="font-serif text-sm w-1/2 py-2">{overview}</h1>
-              <h1 className="font-mono ">{vote_average} 💫</h1>
-              <button className="rounded-full py-2 text-black bg-white px-3 mt-6 text-2xl font-serif">
-                Watch Now!
-              </button>
-            </div>
-          </div>
-          <div className="-z-10 relative">
-            <img
-              className="w-screen h-[830px] "
-              src={POSTER_PATH + backdrop_path}
-              alt="poster_image"
-            />
-          </div>
-        </>
-      )}
+      <div className="text-white text-3xl w-full h-[830px] absolute  bg-gradient-to-t from-black">
+        <div className="mt-[250px] ml-24">
+          <h1 className="font-mono ">HD 2024</h1>
+          <h1 className="font-serif text-6xl py-6">{title}</h1>
+          <h1 className="font-serif text-sm w-1/2 py-2">{overview}</h1>
+          <h1 className="font-mono ">{vote_average} 💫</h1>
+          <button className="rounded-full py-2 text-black bg-white px-3 mt-6 text-2xl font-serif">
+            Watch Now!
+          </button>
+        </div>
+      </div>
+      <div className="-z-10 relative">
+        <img
+          className="w-screen h-[830px] "
+          src={POSTER_PATH + backdrop_path}
+          alt="poster_image"
+        />
+      </div>
       <div className=" p-4  z-10 bg-black">
         <h1 className="text-white text-4xl py-4  pb-10">Recommended</h1>
         <div className="flex overflow-x-scroll no-scrollbar">
